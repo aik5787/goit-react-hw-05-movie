0007f3586b458237a39d4d8c2adbc13a22b81971
Aleksandr <aik5787@gmail.com>
2023-10-03 22:24:02 -0400

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
+import React, { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import HomePage from './Pages/HomePage/HomePage';
-import Movies from './Pages/Movies/Movies';
-import MovieDetails from './Pages/MoviesDetails/MoviesDetails';
-import Cast from './Pages//Cast/Cast';
-import Reviews from './Pages/Reviews/Reviews';
+
+const HomePage = lazy(() => import('./Pages/HomePage/HomePage'));
+const Movies = lazy(() => import('./Pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('./Pages/MoviesDetails/MoviesDetails'));
+const Cast = lazy(() => import('./Pages/Cast/Cast'));
+const Reviews = lazy(() => import('./Pages/Reviews/Reviews'));
+
+// import HomePage from './Pages/HomePage/HomePage';
+// import Movies from './Pages/Movies/Movies';
+// import MovieDetails from './Pages/MoviesDetails/MoviesDetails';
+// import Cast from './Pages/Cast/Cast';
+// import Reviews from './Pages/Reviews/Reviews';
 
 const App = () => {
   return (
